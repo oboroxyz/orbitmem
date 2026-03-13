641d6a70e4e586b77d84cc392363a7a6540f415f
yujiym <yuji.ym@gmail.com>
2026-03-13 10:57:15 +0900

    feat(sdk): export AESEngine from main entry

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -4,7 +4,7 @@ export { DEFAULT_NETWORK, getNetwork, NETWORKS } from "./contracts.js";
 export { createOrbitDBInstance, createVault } from "./data/index.js";
 export { createDiscoveryLayer, MockRegistry, OnChainRegistry } from "./discovery/index.js";
 // Layer-level exports for advanced usage
-export { createEncryptionLayer } from "./encryption/index.js";
+export { AESEngine, createEncryptionLayer } from "./encryption/index.js";
 export { createIdentityLayer } from "./identity/index.js";
 export { createPersistenceLayer } from "./persistence/index.js";
 export { createTransportLayer } from "./transport/index.js";
